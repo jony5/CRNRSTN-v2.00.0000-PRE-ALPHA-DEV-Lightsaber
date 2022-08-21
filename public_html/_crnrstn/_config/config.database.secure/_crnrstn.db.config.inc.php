1829f8fee541e4f4501e1335330d8ae0baab9756
jony5 <j5@jony5.com>
2022-08-21 12:19:18 -0400

    Low level error handling improvements/updates for testing.

--- public_html/_crnrstn/_config/config.database.secure/_crnrstn.db.config.inc.php
+++ public_html/_crnrstn/_config/config.database.secure/_crnrstn.db.config.inc.php
@@ -4,7 +4,7 @@
 // INITIALIZE DATABASE FUNCTIONALITY FOR EACH ENVIRONMENT.
 // $this->oMYSQLI_CONN_MGR->add_connection([environment-key], [db-host], [db-user-name], [db-user-pswd], [db-database-name], [optional-db-port]);
 $this->oMYSQLI_CONN_MGR->add_connection('LOCALHOST_MACBOOKPRO', 'localhost', 'jony5_stage', 'aXNTPxGPeLRwYzTS', 'jony5_stage','3306');
-$this->oMYSQLI_CONN_MGR->add_connection('LOCALHOST_CHAD_MACBOOKPRO', 'localhost', '_jony5_stage', 'aXNTPxGPeLRwYzTS', 'jony5_stage','3306');
+$this->oMYSQLI_CONN_MGR->add_connection('LOCALHOST_CHAD_MACBOOKPRO', 'localhost', 'jony5_stage', 'aXNTPxGPeLRwYzTS', 'jony5_stage','3306');
 $this->oMYSQLI_CONN_MGR->add_connection('BLUEHOST', 'localhost', 'jonyfivc_prod01', 'password123456789', 'jonyfivc_prod01','3306');
 $this->oMYSQLI_CONN_MGR->add_connection('BLUEHOST_WWW', 'localhost', 'jonyfivc_prod01', 'password123456789', 'jonyfivc_prod01','3306');
 //$this->oMYSQLI_CONN_MGR->add_connection('BLUEHOST_SUB', 'localhost', 'evifwebc_jony5', 'password123456789', 'evifwebc_jony5','3306');
