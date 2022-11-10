c126fb3a23bca0c775671b80a99dbfe1cb2809ac
jony5 <j5@jony5.com>
2022-11-10 01:07:08 -0500

    Remove email compatibility issue...base64 encoded media data.

--- public_html/_crnrstn/class/user/crnrstn.user.inc.php
+++ public_html/_crnrstn/class/user/crnrstn.user.inc.php
@@ -9752,27 +9752,34 @@ ACCESS TYPE: SYSTEM LEVEL ACCESS
 
                     if($valid_url){
 
-                        return '<a href="' . $this->oCRNRSTN->return_sticky_link($url, $tmp_sticky_link_meta) . '" target="' . $target . '"><img src="' . $this->oCRNRSTN_ASSET_MGR->return_creative('SOCIAL_' . $tmp_social_media_data_key, CRNRSTN_UI_IMG_BASE64) . '" width="' . $tmp_social_img_width . '" height="' . $tmp_social_img_height . '" alt="' . $tmp_social_img_alt . '" title="' . $tmp_social_img_title .'" border="0" style="border: 0;"></a>';
+                        return $this->oCRNRSTN->return_system_image('SOCIAL_' . $tmp_social_media_data_key, $tmp_social_img_width, $tmp_social_img_height, $this->oCRNRSTN->return_sticky_link($url, $tmp_sticky_link_meta), $tmp_social_img_alt, $tmp_social_img_title, $target, CRNRSTN_UI_IMG_HTML_WRAPPED);
+                        //return '<a href="' . $this->oCRNRSTN->return_sticky_link($url, $tmp_sticky_link_meta) . '" target="' . $target . '"><img src="' . $this->oCRNRSTN_ASSET_MGR->return_creative('SOCIAL_' . $tmp_social_media_data_key, CRNRSTN_UI_IMG_BASE64) . '" width="' . $tmp_social_img_width . '" height="' . $tmp_social_img_height . '" alt="' . $tmp_social_img_alt . '" title="' . $tmp_social_img_title .'" border="0" style="border: 0;"></a>';
 
                     }
 
-                    return '<a href="#" target="_self"><img src="' . $this->oCRNRSTN_ASSET_MGR->return_creative('SOCIAL_' . $tmp_social_media_data_key, CRNRSTN_UI_IMG_BASE64) . '" width="' . $tmp_social_img_width . '" height="' . $tmp_social_img_height . '" alt="' . $tmp_social_img_alt . '" title="' . $tmp_social_img_title .'" border="0" style="border: 0;"></a>';
+                    return $this->oCRNRSTN->return_system_image('SOCIAL_' . $tmp_social_media_data_key, $tmp_social_img_width, $tmp_social_img_height, '#', $tmp_social_img_alt, $tmp_social_img_title, '_self', CRNRSTN_UI_IMG_HTML_WRAPPED);
+                    //return '<a href="#" target="_self"><img src="' . $this->oCRNRSTN_ASSET_MGR->return_creative('SOCIAL_' . $tmp_social_media_data_key, CRNRSTN_UI_IMG_BASE64) . '" width="' . $tmp_social_img_width . '" height="' . $tmp_social_img_height . '" alt="' . $tmp_social_img_alt . '" title="' . $tmp_social_img_title .'" border="0" style="border: 0;"></a>';
 
                 }
 
                 if($this->oCRNRSTN->tmp_restrict_this_image_sprite_media_constant($tmp_social_media_endpoint)){
 
+                    //    public function return_system_image($media_element_key, $width = NULL, $height = NULL, $hyperlink = NULL, $alt = NULL, $title = NULL, $target = NULL, $image_output_mode = NULL){
+                    //return $this->oCRNRSTN->return_system_image('SOCIAL_' . $tmp_social_media_data_key, $tmp_social_img_width, $tmp_social_img_height, $this->oCRNRSTN->return_sticky_link($url, $tmp_sticky_link_meta), $tmp_social_img_alt, $tmp_social_img_title, $target, CRNRSTN_UI_IMG_HTML_WRAPPED);
                     //return $this->oCRNRSTN->return_system_image('CRNRSTN_LOGO', '', 250, NULL, NULL, NULL, NULL, CRNRSTN_UI_IMG_HTML_WRAPPED);
 
                     if($valid_url){
 
                         $tmp_note = '<!-- CRNRSTN :: v' . $this->version_crnrstn() . ' :: Graceful degradation to $email_channel=true until ' . $tmp_social_media_endpoint . ' image sprite coordinates can be approved. -->';
-                        return $tmp_note . '<a href="' . $this->oCRNRSTN->return_sticky_link($url, $tmp_sticky_link_meta) . '" target="' . $target . '"><img src="' . $this->oCRNRSTN_ASSET_MGR->return_creative('SOCIAL_' . $tmp_social_media_data_key, CRNRSTN_UI_IMG_BASE64) . '" width="' . $tmp_social_img_width . '" height="' . $tmp_social_img_height . '" alt="' . $tmp_social_img_alt . '" title="' . $tmp_social_img_title .'" border="0" style="border: 0;"></a>';
+
+                        return $tmp_note . $this->oCRNRSTN->return_system_image('SOCIAL_' . $tmp_social_media_data_key, $tmp_social_img_width, $tmp_social_img_height, $this->oCRNRSTN->return_sticky_link($url, $tmp_sticky_link_meta), $tmp_social_img_alt, $tmp_social_img_title, $target, CRNRSTN_UI_IMG_HTML_WRAPPED);
+                        //return $tmp_note . '<a href="' . $this->oCRNRSTN->return_sticky_link($url, $tmp_sticky_link_meta) . '" target="' . $target . '"><img src="' . $this->oCRNRSTN_ASSET_MGR->return_creative('SOCIAL_' . $tmp_social_media_data_key, CRNRSTN_UI_IMG_BASE64) . '" width="' . $tmp_social_img_width . '" height="' . $tmp_social_img_height . '" alt="' . $tmp_social_img_alt . '" title="' . $tmp_social_img_title .'" border="0" style="border: 0;"></a>';
 
                     }
 
                     $tmp_note = '<!-- CRNRSTN :: v' . $this->version_crnrstn() . ' :: Graceful degradation to $email_channel=true until ' . $tmp_social_media_endpoint . ' image sprite coordinates can be approved. -->';
-                    return $tmp_note . '<a href="#" target="_self"><img src="' . $this->oCRNRSTN_ASSET_MGR->return_creative('SOCIAL_' . $tmp_social_media_data_key, CRNRSTN_UI_IMG_BASE64) . '" width="' . $tmp_social_img_width . '" height="' . $tmp_social_img_height . '" alt="' . $tmp_social_img_alt . '" title="' . $tmp_social_img_title .'" border="0" style="border: 0;"></a>';
+                    return $tmp_note . $this->oCRNRSTN->return_system_image('SOCIAL_' . $tmp_social_media_data_key, $tmp_social_img_width, $tmp_social_img_height, '#', $tmp_social_img_alt, $tmp_social_img_title, '_self', CRNRSTN_UI_IMG_HTML_WRAPPED);
+                    //return $tmp_note . '<a href="#" target="_self"><img src="' . $this->oCRNRSTN_ASSET_MGR->return_creative('SOCIAL_' . $tmp_social_media_data_key, CRNRSTN_UI_IMG_BASE64) . '" width="' . $tmp_social_img_width . '" height="' . $tmp_social_img_height . '" alt="' . $tmp_social_img_alt . '" title="' . $tmp_social_img_title .'" border="0" style="border: 0;"></a>';
 
                 }
 
