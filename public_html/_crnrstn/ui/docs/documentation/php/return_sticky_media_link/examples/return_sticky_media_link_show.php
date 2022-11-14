7fa41c9460ad303a289aeedfa247afa79f9ebc08
jony5 <j5@jony5.com>
2022-11-13 23:01:55 -0500

    return_sticky_media_link() example output copy update.

--- public_html/_crnrstn/ui/docs/documentation/php/return_sticky_media_link/examples/return_sticky_media_link_show.php
+++ public_html/_crnrstn/ui/docs/documentation/php/return_sticky_media_link/examples/return_sticky_media_link_show.php
@@ -9,11 +9,11 @@ include_once(CRNRSTN_ROOT . '/_crnrstn.config.inc.php');
 // HTML STRING DATA FOR A SMALL SOUNDCLOUD MEDIA ICON WITH
 // A STICKY LINK IS RETURNED FOR USE IN EMAIL (OR WHENEVER A
 // SIMPLE ANCHOR TAG WRAPPED <IMG> IS DESIRED).
-echo 'HTML Email Compatible:<br>';
+echo 'HTML Email Compatible (small)<br>';
 echo $oCRNRSTN->return_sticky_media_link('SOUNDCLOUD_SMALL', 'https://soundcloud.com/jonathan-harris-772368100', '_blank', true);
 
 echo '<div class="crnrstn_cb_20"></div>';
-echo 'Image Sprite:<br>';
+echo 'Image Sprite (medium):<br>';
 
 //
 // HTML FOR A MEDIUM SOUNDCLOUD MEDIA ICON WITH A STICKY LINK IS
@@ -23,7 +23,7 @@ echo 'Image Sprite:<br>';
 echo $oCRNRSTN->return_sticky_media_link('SOUNDCLOUD_MEDIUM', 'https://soundcloud.com/jonathan-harris-772368100', '_blank');
 
 echo '<div class="crnrstn_cb_20"></div>';
-echo 'Image Sprite:<br>';
+echo 'Image Sprite (large):<br>';
 echo $oCRNRSTN->return_sticky_media_link('SOUNDCLOUD_LARGE', 'https://soundcloud.com/jonathan-harris-772368100', '_blank');
 
 ?>
