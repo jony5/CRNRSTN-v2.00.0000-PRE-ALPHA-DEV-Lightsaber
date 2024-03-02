4842d490e27ad4f145fbd3c0fda7b7ae1a4b6a75
jony5 <j5@jony5.com>
2024-03-02 06:38:02 -0500

    jony5 dot com (2023) search integrations setup...in flux.

--- public_html/_archives/jony5/2023/_files/common/classes/precious_things.inc.php
+++ public_html/_archives/jony5/2023/_files/common/classes/precious_things.inc.php
@@ -9276,6 +9276,31 @@ class bringer_of_the_precious_things {
     }
 
     //
+    // SOURCE :: https://www.php.net/manual/en/function.str-split.php
+    // AUTHOR :: qeremy [atta] gmail [dotta] com :: https://www.php.net/manual/en/function.str-split.php#113274
+    public function str_split_unicode($str, $length = 1){
+
+        $tmp = preg_split('~~u', $str, -1, PREG_SPLIT_NO_EMPTY);
+
+        if($length > 1){
+
+            $chunks = array_chunk($tmp, $length);
+
+            foreach($chunks as $i => $chunk){
+
+                $chunks[$i] = join('', (array)$chunk);
+
+            }
+
+            $tmp = $chunks;
+
+        }
+
+        return $tmp;
+
+    }
+
+    //
     // SOURCE :: https://www.php.net/manual/en/function.rtrim.php
     // AUTHOR :: pinkgothic at gmail dot com :: https://www.php.net/manual/en/function.rtrim.php#95802
     public function strrtrim($message, $strip){
