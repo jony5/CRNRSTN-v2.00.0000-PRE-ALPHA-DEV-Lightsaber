b1db41d78acac900d659359209729d48b0ff7881
Jonathan 'J5' Harris <J00000101@GMAIL.COM>
2024-06-14 08:48:50 -0400

    I will ❤ you always, my dear. A site wide clean up is under way with optimizations for better performance, Lightbox photo gallery content customizations, and greater ease of implementation of the original Lightbox gallery kit.

--- public_html/_archives/jony5/2023/_files/about/work/skills/index.php
+++ public_html/_archives/jony5/2023/_files/about/work/skills/index.php
@@ -1,70 +1,77 @@
 <?php
-
-/* 
-// J5
+/*
+// 5 ::
 // Code is Poetry */
 require('_crnrstn.root.inc.php');
 include_once($CRNRSTN_ROOT . '_crnrstn.config.inc.php');
-require($oCRNRSTN_ENV->getEnvParam('DOCUMENT_ROOT').$oCRNRSTN_ENV->getEnvParam('DOCUMENT_ROOT_DIR').'/common/inc/session/session.inc.php');
+
+//
+// INITIALIZE WEB PAGE
+// HTTP/S AND DIRECTORY
+// PATH ROOTS.
+//
+// Saturday, June 8, 2024 @ 1320 hrs.
+$tmp_root_path = $oCRNRSTN_ENV->getEnvParam('DOCUMENT_ROOT') . $oCRNRSTN_ENV->getEnvParam('DOCUMENT_ROOT_DIR');
+$tmp_http_root = $oCRNRSTN_ENV->getEnvParam('ROOT_PATH_CLIENT_HTTP') . $oCRNRSTN_ENV->getEnvParam('ROOT_PATH_CLIENT_HTTP_DIR');
+require($tmp_root_path . '/common/inc/session/session.inc.php');
 
 ?>
 <!doctype html>
 <html lang="en">
 <head>
-<?php
-require($oCRNRSTN_ENV->getEnvParam('DOCUMENT_ROOT').$oCRNRSTN_ENV->getEnvParam('DOCUMENT_ROOT_DIR').'/common/inc/head/head.inc.php');
-?>
+<?php require($tmp_root_path . '/common/inc/head/head.inc.php'); ?>
 </head>
-
 <body>
-<?php
-	require($oCRNRSTN_ENV->getEnvParam('DOCUMENT_ROOT').$oCRNRSTN_ENV->getEnvParam('DOCUMENT_ROOT_DIR').'/common/inc/contact/contact.inc.php');
+    <?php
+
+	require($tmp_root_path . '/common/inc/contact/contact.inc.php');
+
 	?>
-<div id="body_wrapper">
+    <div id="body_wrapper">
 	<!-- HEAD CONTENT -->
 	<?php
-	require($oCRNRSTN_ENV->getEnvParam('DOCUMENT_ROOT').$oCRNRSTN_ENV->getEnvParam('DOCUMENT_ROOT_DIR').'/common/inc/nav/topnav.inc.php');
+
+	require($tmp_root_path . '/common/inc/nav/topnav.inc.php');
+
 	?>
 	<div class="cb"></div>
-    
     <!-- LIFESTYLE BANNER -->
     <?php
-    require($oCRNRSTN_ENV->getEnvParam('DOCUMENT_ROOT').$oCRNRSTN_ENV->getEnvParam('DOCUMENT_ROOT_DIR').'/common/inc/lifestyle/banner_component.inc.php');
-    ?>
 
-    <div id="banner_lifestyle_dropshadow" style="background-image:url(<?php echo $oCRNRSTN_ENV->getEnvParam('ROOT_PATH_CLIENT_HTTP').$oCRNRSTN_ENV->getEnvParam('ROOT_PATH_CLIENT_HTTP_DIR'); ?>common/imgs/dropshadow.gif);">
-    	<div id="banner_lifestyle_dropshadow_corner"><img src="<?php echo $oCRNRSTN_ENV->getEnvParam('ROOT_PATH_CLIENT_HTTP').$oCRNRSTN_ENV->getEnvParam('ROOT_PATH_CLIENT_HTTP_DIR'); ?>common/imgs/dropshadow_corner.gif" width="6" height="6" alt=""></div>
+    require($tmp_root_path . '/common/inc/lifestyle/banner_component.inc.php');
+
+    ?>
+    <div id="banner_lifestyle_dropshadow" style="background-image:url(<?php echo $tmp_http_root; ?>common/imgs/dropshadow.gif);">
+    	<div id="banner_lifestyle_dropshadow_corner"><img src="<?php echo $tmp_http_root; ?>common/imgs/dropshadow_corner.gif" width="6" height="6" alt=""></div>
     </div>
-    
+
     <div id="user_transaction_wrapper" class="user_transaction_wrapper" style="display:none;">
         <div class="user_transaction_content">
             <div id="user_transaction_status_msg" class="<?php echo $oUSER->transStatusMessage_ARRAY[0]; ?>"><?php echo $oUSER->transStatusMessage_ARRAY[1]; ?></div>
         </div>
     </div>
-    
+
     <!-- SUB NAV -->
     <div id="subnav_wrapper">
-    	<div class="subnav_lnk_wrapper"><a href="<?php echo $oCRNRSTN_ENV->getEnvParam('ROOT_PATH_CLIENT_HTTP').$oCRNRSTN_ENV->getEnvParam('ROOT_PATH_CLIENT_HTTP_DIR'); ?>about/bio/professional/" target="_self">bio</a></div>
+    	<div class="subnav_lnk_wrapper"><a href="<?php echo $tmp_http_root; ?>about/bio/professional/" target="_self">bio</a></div>
         <div class="subnav_lnk_wrapper sel">work</div>
         <div class="cb"></div>
     </div>
-    
+
     <div class="cb_30"></div>
     <!-- PAGE CONTENT -->
     <div id="content_wrapper">
     	<div id="vert_nav_wrapper">
-    		<div class="vert_nav_lnk_wrapper"><a href="<?php echo $oCRNRSTN_ENV->getEnvParam('ROOT_PATH_CLIENT_HTTP').$oCRNRSTN_ENV->getEnvParam('ROOT_PATH_CLIENT_HTTP_DIR'); ?>about/work/highlights/" target="_self">highlights</a></div>
-            <div class="vert_nav_lnk_wrapper"><a href="<?php echo $oCRNRSTN_ENV->getEnvParam('ROOT_PATH_CLIENT_HTTP').$oCRNRSTN_ENV->getEnvParam('ROOT_PATH_CLIENT_HTTP_DIR'); ?>about/work/experience/" target="_self">experience</a></div>
+    		<div class="vert_nav_lnk_wrapper"><a href="<?php echo $tmp_http_root; ?>about/work/highlights/" target="_self">highlights</a></div>
+            <div class="vert_nav_lnk_wrapper"><a href="<?php echo $tmp_http_root; ?>about/work/experience/" target="_self">experience</a></div>
             <div class="vert_nav_lnk_wrapper sel">skills</a></div>
-    		<div class="vert_nav_lnk_wrapper"><a href="<?php echo $oCRNRSTN_ENV->getEnvParam('ROOT_PATH_CLIENT_HTTP').$oCRNRSTN_ENV->getEnvParam('ROOT_PATH_CLIENT_HTTP_DIR'); ?>about/work/resume/?v=420" target="_self">resume</a></div>
+    		<div class="vert_nav_lnk_wrapper"><a href="<?php echo $tmp_http_root; ?>about/work/resume/?v=420" target="_self">resume</a></div>
     	</div>
-    
     	<div id="content">
             <div class="content_title">Business &amp; Technology</div>
             <div class="content_copy">
             	<div class="col" style="width:900px;">
-           			<p>
-                    <ul>
+           			<p><ul>
                     <li>SMS Promotions :: strategy and execution</li>
                     <li>banner ad promotions :: strategy and execution</li>
                     <li>landing page promotions :: strategy and execution</li>
@@ -83,11 +90,11 @@ require($oCRNRSTN_ENV->getEnvParam('DOCUMENT_ROOT').$oCRNRSTN_ENV->getEnvParam('
                     <li>Computer (Mac/PC) diagnostics and repair</li>
                     <li>Logistics/planning for activities with large groups of people</li>
                     <li>brewing coffee (certified barista for the Starbucks Corporation)</li>
-    				</ul>
-                    </p>
+    				</ul></p>
+
                 </div>
             </div>
-            
+
             <div class="cb"></div>
             <div class="content_hr"></div>
             <div class="content_title">Computers & Electronics</div>
@@ -96,8 +103,7 @@ require($oCRNRSTN_ENV->getEnvParam('DOCUMENT_ROOT').$oCRNRSTN_ENV->getEnvParam('
            			<p>No, I will not fix your computer :)</p>
                 </div>
             </div>
-            
-            
+
             <div class="cb"></div>
             <div class="content_hr"></div>
             <div class="content_title">People</div>
@@ -106,36 +112,50 @@ require($oCRNRSTN_ENV->getEnvParam('DOCUMENT_ROOT').$oCRNRSTN_ENV->getEnvParam('
            			<p>I have people skills!</p>
                 </div>
             </div>
-            
+
             <div class="cb"></div>
             <div class="content_hr"></div>
             <div class="content_title">Animals & the Outdoors</div>
             <div class="content_copy">
             	<div class="col" style="width:900px;">
-           			<p>I enjoy animals and the outdoors. If I can mix both of these items with work...e.g. putting a presentation together from the comfort of a sunny patio or coding in a courtyard with my dog...all the better! As an Eagle Scout and Vigil honor brother in the Order of the Arrow, the connection that I have to the outdoors is respected, but I don't over do it. I've spent more than my fare share of time in the woods on official BSA organizational business. ^_^</p>
+           			<p>I enjoy animals and the outdoors. If I can mix both of these items 
+                    with work...e.g. putting a presentation together from the comfort of 
+                    a sunny patio or coding in a courtyard with my dog...all the better! 
+                    As an Eagle Scout and Vigil honor brother in the Order of the Arrow, 
+                    the connection that I have to the outdoors is respected, but I don't 
+                    over do it. I've spent more than my fare share of time in the woods 
+                    on official BSA organizational business. ^_^</p>
+
                 </div>
             </div>
-            
+
             <div class="cb"></div>
             <div class="content_hr"></div>
             <div class="content_title">Mechanized Systems</div>
             <div class="content_copy">
             	<div class="col" style="width:900px;">
-					<p>Does the contraption you're holding there have it's own mechanical system of operation? Can I see that when you get a sec?</p>
+					<p>Does the contraption you're holding there have it's own 
+                    mechanical system of operation? Can I see that when you get a sec?
+
+                    <br><br>
+                    Yes, I am a really big fan of the Transformers franchise...even 
+                    since the 80's.</p>
+
                 </div>
             </div>
-            
+
     	</div><!-- END PAGE CONTENT -->
-    
+
     </div>
-    
+
     <div class="cb_30"></div>
     <?php
-	require($oCRNRSTN_ENV->getEnvParam('DOCUMENT_ROOT').$oCRNRSTN_ENV->getEnvParam('DOCUMENT_ROOT_DIR').'/common/inc/footer/footer.inc.php');
+
+	require($tmp_root_path . '/common/inc/footer/footer.inc.php');
+
 	?>
     <div class="cb_50"></div>
-    
 
-</div>
+    </div>
 </body>
 </html>
