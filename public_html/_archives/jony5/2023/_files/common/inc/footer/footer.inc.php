ebb6d04cc1b5bbaf463ac04b172f1c013d63a313
Jonathan 'J5' Harris <J00000101@GMAIL.COM>
2024-06-14 08:55:09 -0400

    I will ❤ you always, my dear. A site wide clean up is under way with optimizations for better performance, Lightbox photo gallery content customizations, and greater ease of implementation of the original Lightbox gallery kit.

--- public_html/_archives/jony5/2023/_files/common/inc/footer/footer.inc.php
+++ public_html/_archives/jony5/2023/_files/common/inc/footer/footer.inc.php
@@ -1,31 +1,40 @@
 <?php
 /* 
-// J5
+// 5 ::
 // Code is Poetry */
+//
+// INITIALIZE WEB PAGE
+// HTTP/S AND DIRECTORY
+// PATH ROOTS.
+//
+// Friday, June 7, 2024 @ 2318 hrs.
+$tmp_http_dir = $oCRNRSTN_ENV->getEnvParam('ROOT_PATH_CLIENT_HTTP_DIR');
+$tmp_root_path = $oCRNRSTN_ENV->getEnvParam('DOCUMENT_ROOT') . $oCRNRSTN_ENV->getEnvParam('DOCUMENT_ROOT_DIR');
+$tmp_http_root = $oCRNRSTN_ENV->getEnvParam('ROOT_PATH_CLIENT_HTTP') . $tmp_http_dir;
 
 ?>
     <div id="footer_wrapper">
     	<div id="footer-copyright">&copy; <?php  echo date("Y"); ?> Jonathan '5' Harris :: All Rights Reserved.</div>
-        <div id="5_footer"><a href="<?php echo $oCRNRSTN_ENV->getEnvParam('ROOT_PATH_CLIENT_HTTP').$oCRNRSTN_ENV->getEnvParam('ROOT_PATH_CLIENT_HTTP_DIR'); ?>" target="_self"><img src="<?php echo $oCRNRSTN_ENV->getEnvParam('ROOT_PATH_CLIENT_HTTP').$oCRNRSTN_ENV->getEnvParam('ROOT_PATH_CLIENT_HTTP_DIR'); ?>common/imgs/5.png" width="20" height="20" alt="J5" title="J5" /></a></div>
+        <div id="5_footer"><a href="<?php echo $tmp_http_root; ?>" target="_self"><img src="<?php echo $tmp_http_root; ?>common/imgs/5.png" width="40" height="40" alt="J5" title="J5" /></a></div>
         <div id="bassdrive_standard_for_measurement"></div>
     </div>
-    <div id="banner_endpoint" class="hidden">
-		<?php echo $oCRNRSTN_ENV->getEnvParam('ROOT_PATH_CLIENT_HTTP_DIR'); ?>common/inc/lifestyle/banner_auto_rotate.inc.php
-    </div>
-
-    <div id="ROOT_PATH_CLIENT_HTTP_DIR" class="hidden"><?php echo $oCRNRSTN_ENV->getEnvParam('ROOT_PATH_CLIENT_HTTP_DIR'); ?></div>
+    <div id="banner_endpoint" class="hidden"><?php echo $tmp_http_dir; ?>common/inc/lifestyle/banner_auto_rotate.inc.php</div>
+    <div id="ROOT_PATH_CLIENT_HTTP_DIR" class="hidden"><?php echo $tmp_http_dir; ?></div>
     <div id="banner_cache" class="hidden"><?php
-        include($oCRNRSTN_ENV->getEnvParam('DOCUMENT_ROOT').$oCRNRSTN_ENV->getEnvParam('DOCUMENT_ROOT_DIR').'/common/inc/lifestyle/banner.inc.php');
+
+        include($tmp_root_path . '/common/inc/lifestyle/banner.inc.php');
+
         ?></div>
     <div id="banner_cache_slower_conn" class="hidden"><?php
-        include($oCRNRSTN_ENV->getEnvParam('DOCUMENT_ROOT').$oCRNRSTN_ENV->getEnvParam('DOCUMENT_ROOT_DIR').'/common/inc/lifestyle/banner.inc.php');
+
+        include($tmp_root_path . '/common/inc/lifestyle/banner.inc.php');
+
         ?></div>
     <div id="cache_bust" class="hidden"></div>
     <div id="cache_bust_banner" class="hidden"></div>
     <div id="the_bassdrive_situation" class="hidden"><?php echo $oUSER->returnBassdriveSituation(); ?></div>
     <div id="wall_time" class="hidden">0:00:00</div>
-    <div id="ajax_root" class="hidden"><?php echo $oCRNRSTN_ENV->getEnvParam('ROOT_PATH_CLIENT_HTTP').$oCRNRSTN_ENV->getEnvParam('ROOT_PATH_CLIENT_HTTP_DIR'); ?></div>
-
+    <div id="ajax_root" class="hidden"><?php echo $tmp_http_root; ?></div>
     <div id="banner_mode_track" class="hidden">PLAY</div>
     <div id="script_popup_lock" class="hidden">OFF</div>
     <div id="animation_00_delay" class="hidden">J5</div>
@@ -43,6 +52,7 @@
         //if($tmp_gallery_overlay_BOOL){
           echo '<div id="overlay" style="display:none;"></div>';
         //}
+
     }
 
     if(isset($_GET['vv'])) {
@@ -61,17 +71,15 @@
         echo '
     <div id="page_scroll_to" style="display:none;">' . $tmp_scroll_ID . '</div>';
 
-
     }
 
     ?>
 
     <div class="hidden"><?php echo date("Y-m-d H:i:s", time()); ?></div>
-    <script src="<?php echo $oCRNRSTN_ENV->getEnvParam('ROOT_PATH_CLIENT_HTTP').$oCRNRSTN_ENV->getEnvParam('ROOT_PATH_CLIENT_HTTP_DIR'); ?>common/js/_lib/frameworks/lightbox/2.11.1/js/lightbox-plus-jquery.min.js"></script>
+    <script src="<?php echo $tmp_http_root; ?>common/js/_lib/frameworks/lightbox/2.11.1/js/lightbox-plus-jquery.min.js"></script>
 <?php
 
 /*
-
 <script>
   (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
@@ -82,6 +90,7 @@
   ga('send', 'pageview');
 
 </script>
+
 */
 
 ?>
