93d831a7a10cc6bf63940f0fedc719a2b576a57c
Jonathan 'J5' Harris <J00000101@GMAIL.COM>
2024-06-14 09:14:43 -0400

    I will ❤ you always, my dear. A site wide clean up is under way with optimizations for better performance, Lightbox photo gallery content customizations, and greater ease of implementation of the original Lightbox gallery kit.

--- public_html/_archives/jony5/2023/_files/social/networking/twitter/index.php
+++ public_html/_archives/jony5/2023/_files/social/networking/twitter/index.php
@@ -1,91 +1,115 @@
 <?php
-
-/* 
-// J5
+/*
+// 5 ::
 // Code is Poetry */
 require('_crnrstn.root.inc.php');
 include_once($CRNRSTN_ROOT . '_crnrstn.config.inc.php');
-require($oCRNRSTN_ENV->getEnvParam('DOCUMENT_ROOT').$oCRNRSTN_ENV->getEnvParam('DOCUMENT_ROOT_DIR').'/common/inc/session/session.inc.php');
+
+//
+// INITIALIZE WEB PAGE
+// HTTP/S AND DIRECTORY
+// PATH ROOTS.
+//
+// Saturday, June 8, 2024 @ 1511 hrs.
+$tmp_root_path = $oCRNRSTN_ENV->getEnvParam('DOCUMENT_ROOT') . $oCRNRSTN_ENV->getEnvParam('DOCUMENT_ROOT_DIR');
+$tmp_http_root = $oCRNRSTN_ENV->getEnvParam('ROOT_PATH_CLIENT_HTTP') . $oCRNRSTN_ENV->getEnvParam('ROOT_PATH_CLIENT_HTTP_DIR');
+require($tmp_root_path . '/common/inc/session/session.inc.php');
 
 ?>
 <!doctype html>
 <html lang="en">
 <head>
 <?php
-require($oCRNRSTN_ENV->getEnvParam('DOCUMENT_ROOT').$oCRNRSTN_ENV->getEnvParam('DOCUMENT_ROOT_DIR').'/common/inc/head/head.inc.php');
+require($tmp_root_path . '/common/inc/head/head.inc.php');
 ?>
 </head>
-
 <body>
-<?php
-	require($oCRNRSTN_ENV->getEnvParam('DOCUMENT_ROOT').$oCRNRSTN_ENV->getEnvParam('DOCUMENT_ROOT_DIR').'/common/inc/contact/contact.inc.php');
+    <?php
+
+	require($tmp_root_path . '/common/inc/contact/contact.inc.php');
+
 	?>
-<div id="body_wrapper">
+    <div id="body_wrapper">
 	<!-- HEAD CONTENT -->
 	<?php
-	require($oCRNRSTN_ENV->getEnvParam('DOCUMENT_ROOT').$oCRNRSTN_ENV->getEnvParam('DOCUMENT_ROOT_DIR').'/common/inc/nav/topnav.inc.php');
+
+	require($tmp_root_path . '/common/inc/nav/topnav.inc.php');
+
 	?>
 	<div class="cb"></div>
-    
+
     <!-- LIFESTYLE BANNER -->
     <?php
-    require($oCRNRSTN_ENV->getEnvParam('DOCUMENT_ROOT').$oCRNRSTN_ENV->getEnvParam('DOCUMENT_ROOT_DIR').'/common/inc/lifestyle/banner_component.inc.php');
-    ?>
 
-    <div id="banner_lifestyle_dropshadow" style="background-image:url(<?php echo $oCRNRSTN_ENV->getEnvParam('ROOT_PATH_CLIENT_HTTP').$oCRNRSTN_ENV->getEnvParam('ROOT_PATH_CLIENT_HTTP_DIR'); ?>common/imgs/dropshadow.gif);">
-    	<div id="banner_lifestyle_dropshadow_corner"><img src="<?php echo $oCRNRSTN_ENV->getEnvParam('ROOT_PATH_CLIENT_HTTP').$oCRNRSTN_ENV->getEnvParam('ROOT_PATH_CLIENT_HTTP_DIR'); ?>common/imgs/dropshadow_corner.gif" width="6" height="6" alt=""></div>
+    require($tmp_root_path . '/common/inc/lifestyle/banner_component.inc.php');
+
+    ?>
+    <div id="banner_lifestyle_dropshadow" style="background-image:url(<?php echo $tmp_http_root; ?>common/imgs/dropshadow.gif);">
+    	<div id="banner_lifestyle_dropshadow_corner"><img src="<?php echo $tmp_http_root; ?>common/imgs/dropshadow_corner.gif" width="6" height="6" alt=""></div>
     </div>
-    
+
     <div id="user_transaction_wrapper" class="user_transaction_wrapper" style="display:none;">
         <div class="user_transaction_content">
             <div id="user_transaction_status_msg" class="<?php echo $oUSER->transStatusMessage_ARRAY[0]; ?>"><?php echo $oUSER->transStatusMessage_ARRAY[1]; ?></div>
         </div>
     </div>
-    
+
     <!-- SUB NAV -->
     <div id="subnav_wrapper">
     	<div class="subnav_lnk_wrapper sel">networking</div>
-        <div class="subnav_lnk_wrapper"><a href="<?php echo $oCRNRSTN_ENV->getEnvParam('ROOT_PATH_CLIENT_HTTP').$oCRNRSTN_ENV->getEnvParam('ROOT_PATH_CLIENT_HTTP_DIR'); ?>social/fellowship/podcast/" target="_self">fellowship</a></div>
+        <div class="subnav_lnk_wrapper"><a href="<?php echo $tmp_http_root; ?>social/fellowship/podcast/" target="_self">fellowship</a></div>
         <div class="cb"></div>
     </div>
-    
+
     <div class="cb_30"></div>
     <!-- PAGE CONTENT -->
     <div id="content_wrapper">
     	<div id="vert_nav_wrapper">
-    		<div class="vert_nav_lnk_wrapper"><a href="<?php echo $oCRNRSTN_ENV->getEnvParam('ROOT_PATH_CLIENT_HTTP').$oCRNRSTN_ENV->getEnvParam('ROOT_PATH_CLIENT_HTTP_DIR'); ?>social/networking/facebook/" target="_self">facebook</a></div>
-            <div class="vert_nav_lnk_wrapper"><a href="<?php echo $oCRNRSTN_ENV->getEnvParam('ROOT_PATH_CLIENT_HTTP').$oCRNRSTN_ENV->getEnvParam('ROOT_PATH_CLIENT_HTTP_DIR'); ?>social/networking/googleplus/" target="_self">google+</a></div>
-    		<div class="vert_nav_lnk_wrapper"><a href="<?php echo $oCRNRSTN_ENV->getEnvParam('ROOT_PATH_CLIENT_HTTP').$oCRNRSTN_ENV->getEnvParam('ROOT_PATH_CLIENT_HTTP_DIR'); ?>social/networking/instagram/" target="_self">instagram</a></div>
-    		<div class="vert_nav_lnk_wrapper"><a href="<?php echo $oCRNRSTN_ENV->getEnvParam('ROOT_PATH_CLIENT_HTTP').$oCRNRSTN_ENV->getEnvParam('ROOT_PATH_CLIENT_HTTP_DIR'); ?>social/networking/linkedin/" target="_self">linkedin</a></div>
-            <div class="vert_nav_lnk_wrapper"><a href="<?php echo $oCRNRSTN_ENV->getEnvParam('ROOT_PATH_CLIENT_HTTP').$oCRNRSTN_ENV->getEnvParam('ROOT_PATH_CLIENT_HTTP_DIR'); ?>social/networking/pandora/" target="_self">pandora</a></div>
+    		<div class="vert_nav_lnk_wrapper"><a href="<?php echo $tmp_http_root; ?>social/networking/facebook/" target="_self">facebook</a></div>
+            <div class="vert_nav_lnk_wrapper"><a href="<?php echo $tmp_http_root; ?>social/networking/googleplus/" target="_self">google+</a></div>
+    		<div class="vert_nav_lnk_wrapper"><a href="<?php echo $tmp_http_root; ?>social/networking/instagram/" target="_self">instagram</a></div>
+    		<div class="vert_nav_lnk_wrapper"><a href="<?php echo $tmp_http_root; ?>social/networking/linkedin/" target="_self">linkedin</a></div>
+            <div class="vert_nav_lnk_wrapper"><a href="<?php echo $tmp_http_root; ?>social/networking/pandora/" target="_self">pandora</a></div>
             <div class="vert_nav_lnk_wrapper sel">twitter</div>
-        
-        
         </div>
-    
     	<div id="content">
     		<div class="content_title">Follow me on Twitter<sup style="font-size:60%;">&reg;</sup></div>
             <div class="content_copy">
             	<div class="col" style="width:900px;">
-           			<p>I have two (2) accounts and am taking my time to decide how they are going to be used moving forward. Account #1 (<a href="https://twitter.com/j00000101" target="_blank">@j00000101</a>) was originally created for logging time spend on client billables. Presently, this account is tied to my mobile device and so will occasionally be tweeted to when inspiration strikes.</p>
-                    <p>Twitter<sup style="font-size:60%;">&reg;</sup> account #2 (<a href="https://twitter.com/jony5" target="_blank">@jony5</a>) is accessed primarily from my laptop, and so it seems to be more inclined to get tweets which are nothing more than links to content of interest garnered from time spent perusing the interwebs.</p>
+           			<p>I have two (2) accounts and am taking my 
+                    time to decide how they are going to be 
+                    used moving forward. Account #1 (<a href="https://twitter.com/j00000101" target="_blank">@j00000101</a>) 
+                    was originally created for logging time 
+                    spend on client billables. Presently, 
+                    this account is tied to my mobile device 
+                    and so will occasionally be tweeted to 
+                    when inspiration strikes.</p>
+
+                    <p>Twitter<sup style="font-size:60%;">&reg;</sup> 
+                    account #2 (<a href="https://twitter.com/jony5" target="_blank">@jony5</a>) 
+                    is accessed primarily from my laptop, 
+                    and so it seems to be more inclined to 
+                    get tweets which are nothing more than 
+                    links to content of interest garnered 
+                    from time spent perusing the interwebs.</p>
+
                     <div class="cb_200"></div>
                     <div class="cb_50"></div>
             	</div>
             </div>
-            
-            
+
     	</div><!-- END PAGE CONTENT -->
-    	
+
     </div>
-    
+
     <div class="cb_30"></div>
     <?php
-	require($oCRNRSTN_ENV->getEnvParam('DOCUMENT_ROOT').$oCRNRSTN_ENV->getEnvParam('DOCUMENT_ROOT_DIR').'/common/inc/footer/footer.inc.php');
+
+	require($tmp_root_path . '/common/inc/footer/footer.inc.php');
+
 	?>
     <div class="cb_50"></div>
-    
 
-</div>
+    </div>
 </body>
 </html>
