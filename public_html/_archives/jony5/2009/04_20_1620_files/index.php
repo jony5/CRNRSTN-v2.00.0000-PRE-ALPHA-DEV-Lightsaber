97c9c959f8c592d271b07915bc7db23d95b07e03
Jonathan 'J5' Harris <J00000101@GMAIL.COM>
2024-06-19 07:32:09 -0400

    I will ❤ you always, my dear. A site wide clean up is under way with optimizations for better performance, Lightbox photo gallery content customizations, and greater ease of implementation of the original Lightbox gallery kit.

---
+++ public_html/_archives/jony5/2009/04_20_1620_files/index.php
@@ -0,0 +1,488 @@
+<?php
+	include_once("./root.inc.php");
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head> 
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="description" content="Professional web site development and email marketing by J5." />
+<meta name="keywords" content="j5, jonathan harris, jonathan, email, email marketing, web standards, web design, web development, marketing, CSS, XHTML, email validator, validator, freelance, php, php development, moxie interactive, atlanta, moxie" />
+<meta http-equiv="content-language" content="en" />
+<link rel="stylesheet" type="text/css" media="all" href="common/css/base/reset.css" />
+<link rel="stylesheet" type="text/css" media="all" href="common/css/base/text.css" />
+<link rel="stylesheet" type="text/css" media="all" href="common/css/base/960.css" />
+<link rel="stylesheet" href="common/css/common.css" type="text/css" media="all" />
+
+<title>J5</title>
+</head>
+
+<body>
+<div class="cb_small"></div> 
+<div class="container_16" style="background-color:#FFFFFF;">
+    <div class="cb_small"></div> 
+	<div class="grid_3"> 
+		<h1>J5</h1>
+	</div> 
+	<div class="grid_12" style=" background-color:#000;">
+        <div id="jonathan_lifestyle"><img src="common/imgs/jonathan_00.jpg" width="760" height="199" alt="Jonathan Harris"  title="Jonathan Harris" /></div>
+	</div> 
+    <div class="cb_small"></div> 
+	<div class="grid_3"> 
+		 &nbsp;
+	</div> 
+	<div class="grid_13">
+		<div id="j5_text_pos1" class="grid_4 alpha">
+            <p>I'm Jonathan 'J5' Harris, a web professional living and working in Atlanta, GA. I specialize in email program design & message deliverability. In my free time, I jump head first into e-commerce web development, web standards, usability and visual design.</p>
+            
+            <p>In 2004 I worked as a freelance designer, web application developer and serial entrepreneur. After the implosion of my start-up</p>
+		</div>
+		<div id="j5_text_pos2" class="grid_4 omega">
+            <p>company, I entered the world of interactive marketing and advertising when I accepted a UI developer position with an Atlanta based agency, Moxie Interactive.</p>
+            
+            <p>In 2007 I joined the eCRM team at Moxie and was recently promoted to be the Moxie Interactive eCRM Solutions Engineer. As such, I architect multi-channel data capture and message deliverability systems that support the
+		</div>
+		<div id="j5_text_pos3" class="grid_4 omega" >
+            <p> strategic initiatives of our clients.</p>
+
+            <p>Recently, I have started accepting project proposals for small to medium sized businesses, so please feel free to contact me to see if I can help.</p>
+		</div>  
+ 	</div> 
+  
+    <div class="cb"></div> 
+	<div class="grid_3"> 
+		 &nbsp;
+	</div> 
+	<div class="grid_13">
+		<div id="j5_photo_pos1" class="grid_4 alpha">
+			<img src="common/imgs/j5_00.jpg" width="240" height="180" alt="J5 - 9 weeks" title="J5" /> 
+		</div>
+		<div id="j5_photo_pos2" class="grid_4" omega >
+            <img src="common/imgs/j5_01.jpg" width="240" height="180" alt="J5 - 1 year" title="J5" /> 
+		</div>
+		<div id="j5_photo_pos3" class="grid_4 omega" >
+            <img src="common/imgs/j5_02.jpg" width="240" height="180" alt="J5 - 2.5 years" title="J5" /> 
+		</div>  
+ 	</div> 
+    <div class="cb_small"></div> 
+    
+	<div class="grid_3"> 
+		 &nbsp;
+	</div> 
+	<div class="grid_13">
+		<div class="grid_9 alpha">
+        	<div class="subtitle_wrapper">
+				<h2>projects</h2>
+            </div>
+		</div>
+ 
+		<div class="grid_4 omega">
+        	<div class="subtitle_wrapper">
+           		<h2>drum and bass</h2>
+            </div>
+		</div>  
+ 	</div> 
+    <div class="cb_small"></div> 
+    <div class="grid_3"> 
+		 &nbsp;
+	</div> 
+	<div class="grid_13">
+		<div class="grid_9 alpha">
+        	<div class="project_wrapper">
+            	<div><h3>HTML Email Development - Verizon Wireless</h3></div>
+				<div><img src="common/imgs/project_vzw.jpg" width="504" height="104" alt="Verizon Wireless" title="Verizon Wireless" /></div>
+            </div>
+		</div>
+ 
+		<div class="grid_4 omega">  
+             <p>Featuring live sets from DJ's from all over the world, BassDrive is a 24/7 365 online Drum & Bass radio station. Visit the web site or click here to stream.</p>
+            <div><a href="http://bassdrive.com/v2" target="_blank"><img src="common/imgs/icon_bassdrive.jpg" width="78" height="57" alt="Bassdrive" title="Bassdrive" /></a></div>
+		
+		</div>  
+ 	</div> 
+    <div class="cb_small"></div>
+	   
+ 	<div class="grid_3"> 
+		 &nbsp;
+	</div> 
+	<div class="grid_13">
+		<div class="grid_9 alpha">
+        	<div class="project_wrapper">
+            	<div>
+            	  <h3>Creative Extranet CMS - Moxie Interactive</h3>
+            	</div>
+				<div><img src="common/imgs/project_moxieextranet.jpg" width="504" height="104" alt="Moxie Interactive - Client Extranet" title="Moxie Interactive - Client Extranet" /></div>
+            </div>
+		</div>
+ 
+<div class="grid_4 omega">  
+       	  <div class="subtitle_wrapper" style="padding-bottom:0px; height:60px;"> 
+                    <table cellpadding="0" cellspacing="0" border="0"><tr><td height="40" valign="top"><h2>bookmarks</h2></td><td height="40" valign="top" style="padding-left:20px;"><img src="common/imgs/icon_delicious.jpg" width="25" height="39" alt="Delicious" /></td></tr>
+            		</table>
+          </div>     
+            <table cellpadding="0" cellspacing="0" border="0">   
+                    <tr><td> 
+                    <div class="sub_wrapper">
+                        <div class="item_yin">
+                            <div class="fav_title"><span title="Akamai: Web Application Acceleration and Performance Management"><a href="http://www.akamai.com/" target="_blank">Akamai: Web Application Acceler...</a></span></div>
+                            <div class="fav_tags">tags: <a class="tag" href="http://delicious.com/J00000101/cache">cache</a> <a class="tag" href="http://delicious.com/J00000101/hosting">hosting</a> <a class="tag" href="http://delicious.com/J00000101/edge">edge</a> <a class="tag" href="http://delicious.com/J00000101/streaming">streaming</a>... </div>
+                        </div>
+                        <div class="item_yang">
+                            <div class="fav_title"><span title="Responsys: Recognized Leader in On-Demand Marketing"><a href="http://www.responsys.com/" target="_blank">Responsys: Recognized Leader in...</a></span></div>
+                            <div class="fav_tags">tags: <a class="tag" href="http://delicious.com/J00000101/email">email</a> <a class="tag" href="http://delicious.com/J00000101/marketing">marketing</a> <a class="tag" href="http://delicious.com/J00000101/analytics">analytics</a> <a class="tag" href="http://delicious.com/J00000101/ecrm">ecrm</a> ... </div>
+                        </div>	
+                        <div class="item_yin">
+                            <div class="fav_title"><span title="Omniture: Web Analytics | Online Business  Optimization"><a href="http://www.omniture.com/en/" target="_blank">Omniture: Web Analytics | On...</a></span></div>
+                            <div class="fav_tags">tags: <a class="tag" href="http://delicious.com/J00000101/analytics">analytics</a> <a class="tag" href="http://delicious.com/J00000101/marketing">marketing</a>... </div>
+                        </div>	
+                        <div class="item_yang">
+                            <div class="fav_title"><span title="MooTools: A compact, modular, Object-Oriented JavaScript framework"><a href="http://mootools.net/" target="_blank">MooTools: A compact, mod...</a></span></div>
+                            <div class="fav_tags">tags: <a class="tag" href="http://delicious.com/J00000101/JavaScript">JavaScript</a> <a class="tag" href="http://delicious.com/J00000101/ajax">ajax</a> <a class="tag" href="http://delicious.com/J00000101/framework">framework</a>... </div>
+                        </div>	
+                        <div class="item_yin">
+                            <div class="fav_title"><span title="Prototype JavaScript framework: Ajax and DOM manipulation for dynamic web applications"><a href="http://www.prototypejs.org/" target="_blank">Prototype JavaScript framewo...</a></span></div>
+                            <div class="fav_tags">tags: <a class="tag" href="http://delicious.com/J00000101/ajax">ajax</a> <a class="tag" href="http://delicious.com/J00000101/framework">framework</a> <a class="tag" href="http://delicious.com/J00000101/JavaScript">JavaScript</a>... </div>
+                        </div>
+                        <div class="item_yang">
+                            <div class="fav_title"><span title="Lightbox JS"><a href="http://www.huddletogether.com/projects/lightbox/" target="_blank">Lightbox JS</a></span></div>
+                            <div class="fav_tags">tags: <a class="tag" href="http://delicious.com/J00000101/ajax">ajax</a> <a class="tag" href="http://delicious.com/J00000101/framework">framework</a> <a class="tag" href="http://delicious.com/J00000101/JavaScript">JavaScript</a>... </div>
+                        </div>		
+                      </div>
+                      </td>
+              </tr>
+          </table>  
+  		</div> 
+ 	</div> 
+    <div class="cb_small"></div>    
+
+	
+    <div class="grid_3"> 
+		 &nbsp;
+	</div> 
+	<div class="grid_13">
+		<div class="grid_9 alpha">
+        	<div class="project_wrapper">
+            	<div><h3>e-Commerce/Flash Development - Pix2Flix</h3></div>
+				<div><img src="common/imgs/project_pix2flix.jpg" width="504" height="104" alt="Pix2Flix" title="Pix2Flix" /></div>
+            </div>
+		</div>
+ 
+		<div class="grid_4 omega">  
+
+		</div>  
+ 	</div> 
+    <div class="cb_small"></div>                    
+	<div class="grid_3"> 
+		 &nbsp;
+	</div> 
+	<div class="grid_13">
+		<div class="grid_9 alpha">
+        	<div class="project_wrapper">
+            	<div><h3>Word Press Customization - Hoya Wrestling</h3></div>
+				<div><img src="common/imgs/project_hoyas.jpg" width="504" height="104" alt="Hoya Wrestling" title="Hoya Wrestling" /></div>
+            </div>
+		</div>
+ 
+		<div class="grid_4 omega">  
+ 	</div>  
+ 	</div> 
+    <div class="cb_small"></div> 
+    
+    <div class="grid_3"> 
+		 &nbsp;
+	</div> 
+	<div class="grid_13">
+		<div class="grid_9 alpha">
+        	<div class="project_wrapper">
+            	<div><h3>Joomla! Customization - Saltwater Guide Network</h3></div>
+				<div><img src="common/imgs/project_sgn.jpg" width="504" height="104" alt="Saltwater Guide Network" title="Saltwater Guide Network" /></div>
+            </div>
+		</div>
+ 
+		<div class="grid_4 omega">  
+                
+  		</div>  
+ 	</div> 
+  <div class="cb_small"></div> 
+	
+    <div class="grid_3"> 
+		 &nbsp;
+	</div> 
+	<div class="grid_13">
+		<div class="grid_9 alpha">
+        	<div class="project_wrapper">
+            	<div>
+            	  <h3>Website Design &amp; Development - Christian Students</h3>
+           	  </div>
+				<div><img src="common/imgs/project_cs.jpg" width="504" height="104" alt="Christian Students" title="Christian Students" /></div>
+            </div>
+		</div>
+ 
+		<div class="grid_4 omega">  
+ 
+		</div>  
+ 	</div> 
+    <div class="cb_small"></div> 
+
+    
+    <div class="grid_3"> 
+		 &nbsp;
+	</div> 
+	<div class="grid_13">
+		<div class="grid_9 alpha">
+        	<div class="project_wrapper">
+            	<div>
+            	  <h3>Website Backup Automation</h3>
+            	</div>
+				<div><img src="common/imgs/project_backups.jpg" width="504" height="104" alt="Website Backup Processes" title="Website Backup Processes" /></div>
+            </div>
+		</div>
+ 
+		<div class="grid_4 omega">  
+ 
+		</div>  
+ 	</div> 
+    <div class="cb_small"></div> 
+
+    
+	<div class="grid_3"> 
+		 &nbsp;
+	</div> 
+	<div class="grid_13">
+		<div class="grid_9 alpha">
+        	<div class="project_wrapper">
+            	<div><h3>Website Design &amp; Development - Stephen Harris</h3></div>
+				<div><img src="common/imgs/project_stephenharris.jpg" width="504" height="104" alt="Stephen Harris - Olympic Decathlete" title="Stephen Harris - Olympic Decathlete" /></div>
+            </div>
+		</div>
+ 
+		<div class="grid_4 omega">  
+ 
+		</div>  
+ 	</div> 
+    <div class="cb_small"></div> 
+        
+    <div class="grid_3"> 
+		 &nbsp;
+	</div> 
+	<div class="grid_13">
+		<div class="grid_9 alpha">
+        	<div class="project_wrapper">
+            	<div>
+            	  <h3>Website Development - Uniform Sales of America, Inc.</h3>
+            	</div>
+				<div><img src="common/imgs/project_usa.jpg" width="504" height="104" alt="Uniform Sales of America, Inc." title="Uniform Sales of America, Inc." /></div>
+            </div>
+		</div>
+ 
+		<div class="grid_4 omega">  
+ 
+		</div>  
+ 	</div> 
+    <div class="cb_small"></div> 
+	
+    <div class="grid_3"> 
+		 &nbsp;
+	</div> 
+	<div class="grid_13">
+		<div class="grid_9 alpha">
+        	<div class="project_wrapper">
+            	<div><h3>e-Commerce Development - Showseason Products</h3></div>
+				<div><img src="common/imgs/project_showseason.jpg" width="504" height="104" alt="Showseason Products" title="Showseason Products" /></div>
+            </div>
+		</div>
+ 
+		<div class="grid_4 omega">  
+
+		</div>  
+ 	</div> 
+    <div class="cb_small"></div> 
+    
+	
+    <div class="grid_3"> 
+		 &nbsp;
+	</div> 
+	<div class="grid_13">
+		<div class="grid_9 alpha">
+        	<div class="project_wrapper">
+            	<div><h3>Website Design &amp; Development - The Independent</h3></div>
+				<div><img src="common/imgs/project_indy.jpg" width="504" height="104" alt="The Independent - Booze. Bistro. Billiards" title="The Independent - Booze. Bistro. Billiards" /> </div>
+            </div>
+		</div>
+ 
+		<div class="grid_4 omega">  
+
+		</div>  
+ 	</div> 
+    <div class="cb_small"></div>     
+ 
+	<div class="grid_3"> 
+		 &nbsp;
+	</div> 
+	<div class="grid_13">
+		<div class="grid_9 alpha">
+        	<div class="project_wrapper">
+            	<div><h3>HTML Email Development - Moxie Interactive, Inc.</h3></div>
+				<div><img src="common/imgs/project_moxieemail.jpg" width="504" height="104" alt="Moxie Interactive - HTML Email" title="Moxie Interactive - HTML Email" /></div>
+            </div>
+		</div>
+ 
+		<div class="grid_4 omega">  
+ 
+		</div>  
+ 	</div> 
+    <div class="cb_small"></div> 
+	<div class="grid_3"> 
+		 &nbsp;
+	</div> 
+	<div class="grid_13">
+		<div class="grid_9 alpha">
+        	<div class="project_wrapper">
+            	<div><h3>HTML Email Development - Frederick Swanston, Inc.</h3></div>
+				<div><img src="common/imgs/project_frederickswanston.jpg" width="504" height="104" alt="Frederick Swanston, Inc." title="Frederick Swanston, Inc." /></div>
+            </div>
+		</div>
+ 
+		<div class="grid_4 omega">  
+ 
+		</div>  
+ 	</div> 
+    <div class="cb_small"></div>     
+    
+    <div class="grid_3"> 
+		 &nbsp;
+	</div> 
+	<div class="grid_13">
+		<div class="grid_9 alpha">
+        	<div class="project_wrapper">
+            	<div><h3>HTML Email Development - Student Laundry 101</h3></div>
+				<div><img src="common/imgs/project_sl101.jpg" width="504" height="104" alt="Student Laundry 101" title="Student Laundry 101" /></div>
+            </div>
+		</div>
+ 
+		<div class="grid_4 omega">  
+ 
+		</div>  
+ 	</div> 
+    <div class="cb_small"></div>
+    <div class="grid_3"> 
+		 &nbsp;
+	</div> 
+	<div class="grid_13">
+		<div class="grid_9 alpha">
+        	<div class="project_wrapper">
+            	<div><h3>HTML Email Development - 4Sevens Flashlights and Gear</h3></div>
+				<div><img src="common/imgs/project_4sevens.jpg" width="504" height="104" alt="4Sevens Flashlights and Gear" title="4Sevens Flashlights and Gear" /></div>
+            </div>
+		</div>
+ 
+		<div class="grid_4 omega">  
+ 
+		</div>  
+ 	</div> 
+    <div class="cb_small"></div>
+    <div class="grid_3"> 
+		 &nbsp;
+	</div> 
+	<div class="grid_13">
+		<div class="grid_9 alpha">
+        	<div class="project_wrapper">
+            	<div><h3>Website Design &amp; Development - Fulton County</h3></div>
+				<div><img src="common/imgs/project_dafc.jpg" width="504" height="104" alt="Development Authority of Fulton County" title="Development Authority of Fulton County" /></div>
+            </div>
+		</div>
+ 
+		<div class="grid_4 omega">  
+ 
+		</div>  
+ 	</div> 
+    <div class="cb_small"></div>
+    <div class="grid_3"> 
+		 &nbsp;
+	</div> 
+	<div class="grid_13">
+		<div class="grid_9 alpha">
+        	<div class="project_wrapper">
+            	<div><h3>Portfolio Design and Development - Jess Jimin&eacute;z</h3></div>
+				<div><img src="common/imgs/project_jiminez.jpg" width="504" height="104" alt="Jess Jiminez" title="Jess Jimin&eacute;z" /></div>
+            </div>
+		</div>
+ 
+		<div class="grid_4 omega">  
+ 
+		</div>  
+ 	</div> 
+    <div class="cb_small"></div>     
+       
+   <div class="grid_3"> 
+		 &nbsp;
+	</div> 
+	<div class="grid_13">
+		<div class="grid_9 alpha">
+        	<div class="project_wrapper">
+            	<div>
+            	  <h3>Corporate Directory CMS - Moxie Interactive, Inc.</h3>
+            	</div>
+				<div><img src="common/imgs/project_mfb.jpg" width="504" height="104" alt="Moxie Interactive" title="Moxie Interactive" /></div>
+            </div>
+		</div>
+ 
+		<div class="grid_4 omega">  
+ 
+		</div>  
+ 	</div> 
+    <div class="cb_small"></div>    
+ <div class="grid_3"> 
+		 &nbsp;
+	</div> 
+	<div class="grid_13">
+		<div class="grid_9 alpha">
+        	<div class="project_wrapper">
+            	<div><h3>Website Design &amp; Development - Plansouth</h3></div>
+				<div><img src="common/imgs/project_plansouth.jpg" width="504" height="104" alt="Plansouth" title="Plansouth" /></div>
+            </div>
+		</div>
+ 
+		<div class="grid_4 omega">  
+ 
+		</div>  
+ 	</div> 
+    <div class="cb_small"></div>
+  <div class="grid_3"> 
+		 &nbsp;
+	</div> 
+	<div class="grid_13">
+		<div class="grid_9 alpha">
+        	<div class="project_wrapper">
+            	<div><h3>Website Design &amp; Development - Plantation Homes, LLC</h3></div>
+				<div><img src="common/imgs/project_phllc.jpg" width="504" height="104" alt="Plansouth" title="Plansouth" /></div>
+            </div>
+		</div>
+ 
+		<div class="grid_4 omega">  
+ 
+		</div>  
+ 	</div> 
+    <div class="cb_small"></div>
+ 
+ 
+ 
+ 
+ 
+	</div>
+
+    
+    
+    
+</div><!-- #END 16 -->
+<div class="cb_large"></div>
+<script type="text/javascript">
+var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+</script>
+<script type="text/javascript">
+var pageTracker = _gat._getTracker("UA-2181418-7");
+pageTracker._initData();
+pageTracker._trackPageview();
+</script>
+</body>
+</html>
